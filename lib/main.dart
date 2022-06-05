7cb8d78ffece1370c23f47a878d23cf0018644aa
WillianFarias <williansousafarias@gmail.com>
2022-06-05 16:52:26 -0300

    app finalizado

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,14 @@ class _HomeState extends State<Home> {
   String _informacao = "Digite os valores";
   TextEditingController taxaController = TextEditingController();
   TextEditingController valorBrutoController = TextEditingController();
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
   _limparFormulario() {
     taxaController.text = "";
     valorBrutoController.text = "";
     setState(() {
       _informacao = "Digite os valores";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
@@ -58,6 +60,7 @@ class _HomeState extends State<Home> {
       body: SingleChildScrollView(
         padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               TextFormField(
@@ -69,21 +72,45 @@ class _HomeState extends State<Home> {
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Color.fromARGB(255, 51, 49, 47)),
                 controller: taxaController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Valor da Taxa não estar em branco!";
+                  }
+                  try {
+                    double.parse(value);
+                  } catch (e) {
+                    return "Valor inválido!";
+                  }
+                },
               ),
               TextFormField(
                 keyboardType: TextInputType.number,
                 decoration: InputDecoration(
-                  labelText: "Valor bruto",
+                  labelText: "Valor Bruto",
                   labelStyle: TextStyle(color: Color.fromARGB(255, 51, 49, 47)),
                 ),
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Color.fromARGB(255, 51, 49, 47)),
                 controller: valorBrutoController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return "Valor Bruto não estar em branco!";
+                  }
+                  try {
+                    double.parse(value);
+                  } catch (e) {
+                    return "Valor Bruto inválido!";
+                  }
+                },
               ),
               Padding(
                 padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
                 child: ElevatedButton(
-                  onPressed: () {},
+                  onPressed: () {
+                    if (_formKey.currentState?.validate() == true) {
+                      _calcular();
+                    }
+                  },
                   child: Text("Calcular"),
                   style: ElevatedButton.styleFrom(
                     primary: Color.fromARGB(255, 60, 136, 171),
